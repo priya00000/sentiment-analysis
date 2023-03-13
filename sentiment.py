485419c7c6fb58c30e1782982486bdffb4216722
Priya_Rajput <105284244+priya00000@users.noreply.github.com>
2023-03-13 15:44:00 +0530

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -12,10 +12,10 @@ class TwitterClient(object):
 		Class constructor or initialization method.
 		'''
 		# keys and tokens from the Twitter Dev Console
-		consumer_key = "nQDIj9tj8dugmxMWAleNO008Q"
-		consumer_secret = "71tXuSkHP6PQK10Oo1C85wMyC8X6n3mQ8NRDAaT5W2knSkhzS6"
-		access_token = "1280405575990304768-zSZDvCgFGoy2f0J6OmT4pCMZf0jAw3"
-		access_token_secret = "YkMBe26M5os4NqhUAqap1o1K5kpDX7IY9SN4rlmRSL2tw"
+		consumer_key = "xxxxxxxxx"
+		consumer_secret = "xxxxxxxx"
+		access_token = "xxxxxx-xxxxxx"
+		access_token_secret = "xxxxxx"
 
 		# attempt authentication
 		try:
